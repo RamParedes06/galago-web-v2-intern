54eb9ebdeec61c5d633aa636c78ddbe61b0790f5
bmr13 <bea.rumbaua@aiqueinnovation.com>
2024-02-26 16:39:44 +0800

    navigation bar

---
+++ src/components/ui/NavigationBar.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "../../styles/navigationbar.scss";
+
+function NavigationBar() {
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeColor);
+
+  return (
+    <div className={color ? "nav-container nav-container-bg" : "nav-container"}>
+      <div className="nav-logo">
+        <img src="" alt="Logo" />
+      </div>
+
+      <div className="nav-links">
+        <p>Home</p>
+        <p>Promos</p>
+        <p>Blogs</p>
+        <p>About Us</p>
+        <button className="shadow-sm rounded">Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import '../styles/homepage.scss'
-
+import React from "react";
+import "../styles/homepage.scss";
+import NavigationBar from "../components/ui/NavigationBar";
 
 const Homepage = () => {
   return (
     <div>
-        
+      <NavigationBar />
       <div className="hero-section">
         <div className="hero-details">
-        <h1>Get ready to GO!</h1>
-        <p>Trot the globe or discover the beauty of the Philippines islands. Your next adventure starts here!</p>
+          <h1>Get ready to GO!</h1>
+          <p>
+            Trot the globe or discover the beauty of the Philippines islands.
+            Your next adventure starts here!
+          </p>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
