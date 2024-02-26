b4669dbe6cb0ed50e32a093a8ce01ad772f91d66
Aliyah Sagaban <aliyah.sagaban@aiqueinnovation.com>
2024-02-26 16:17:38 +0800

    added homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/homepage.scss'
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <div className="hero-section">
+        <div className="hero-details">
+        <h1>Get ready to GO!</h1>
+        <p>Trot the globe or discover the beauty of the Philippines islands. Your next adventure starts here!</p>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
