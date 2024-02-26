e4bc7994cbbb5ecdfac23c9705dcdd3d86ed5d8a
Aliyah Sagaban <aliyah.sagaban@aiqueinnovation.com>
2024-02-26 15:44:27 +0800

    initial route pages added

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { createHashRouter } from "react-router-dom";
+import Index from './pages/Index'
+
+const router = createHashRouter([
+    {
+        path: '/',
+        element: <Index/>
+    }
+
+])
+
+export {router}
