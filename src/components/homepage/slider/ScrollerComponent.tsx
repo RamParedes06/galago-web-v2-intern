90855c6e9159ef45160e21326c7049dade55a5fd
Aliyah Sagaban <aliyah.sagaban@aiqueinnovation.com>
2024-02-28 17:59:24 +0800

    finished initial layout of homepage (with slider and footer)

---
+++ src/components/homepage/slider/ScrollerComponent.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useEffect } from "react";
+import slider1 from "../../../resources/homepage/slider1.png";
+import slider2 from "../../../resources/homepage/slider2.png";
+import slider3 from "../../../resources/homepage/slider3.png";
+import "../../../styles/slider.scss";
+
+const ScrollerComponent = () => {
+  // const imagesUrl = ASSETS_API().resources.images;
+
+  useEffect(() => {
+    const scrollers = document.querySelectorAll(".scroller");
+
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      addAnimation();
+    }
+
+    function addAnimation() {
+      scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", "true");
+
+        const scrollerInner = scroller.querySelector(
+          ".scroller__inner"
+        ) as HTMLElement;
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach((item) => {
+          const duplicatedItem = item.cloneNode(true) as HTMLElement;
+          duplicatedItem.setAttribute("aria-hidden", "true");
+          scrollerInner.appendChild(duplicatedItem);
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <div className="scroller w-full" data-direction="right" data-speed="slow">
+      <div className="scroller__inner">
+        <div className="scroller-img">
+          <img src={slider1} alt="" loading="lazy" />
+        </div>
+        <div className="scroller-img">
+          <img src={slider2} alt="" loading="lazy" />
+        </div>
+        <div className="scroller-img">
+          <img src={slider3} alt="" loading="lazy" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollerComponent;
